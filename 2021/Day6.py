3a4c8893025127941f9a56dd511280329406aac3
Filip Lundberg <lundax@gmail.com>
2021-12-06 22:07:40 +0100

    Day6 completed, two algorithms

--- 2021/Day6.py
+++ 2021/Day6.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import DefaultDict, List
 from aocd.models import Puzzle
 
 
@@ -38,6 +38,26 @@ class FishSchool:
         string_state = [str(fish.timer) for fish in self.fishes]
         return ','.join(string_state)
 
+class FishPool:
+    def __init__(self, initial_timers) -> None:
+        self.timer_dict = DefaultDict(int)
+        for day in initial_timers:
+            self.timer_dict[day] += 1
+
+    def step(self):
+        for i in range(-1, 8):
+            self.timer_dict[i] = self.timer_dict[i+1]
+        self.timer_dict[8] = self.timer_dict[-1]
+        self.timer_dict[6] += self.timer_dict[-1]
+
+    def simulate(self, steps):
+        for i in range(steps):
+            self.step()
+    
+    def get_total_fish(self):
+        return sum([self.timer_dict[i] for i in range(9)])
+
+
 if __name__ == "__main__":
     start_timers = [int(x) for x in TEST_DATA.split(',')]
     start_fishes = [LanternFish(x) for x in start_timers]
@@ -58,3 +78,9 @@ if __name__ == "__main__":
 
     puzzle = Puzzle(year=2021, day=6)
     puzzle.answer_a = school.get_total_fish()
+    # school.simulate_a(256-80)
+    pool = FishPool(start_timers)
+    pool.simulate(80)
+    assert(pool.get_total_fish() == school.get_total_fish())
+    pool.simulate(256-80)
+    puzzle.answer_b = pool.get_total_fish()
