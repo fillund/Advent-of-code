6f2ef671d70f56c9b7268848531c9ec3cc78e8d6
Filip Lundberg <lundax@gmail.com>
2024-09-25 15:57:26 +0200

    Day9 partB

--- 2023/Day9.py
+++ 2023/Day9.py
@@ -11,19 +11,23 @@ def running_diff(data: list[int]):
     # Diffs backwards rather than forwards
     return [b-a for a,b in it.pairwise(data)]
 
-def recurse_diff(numbers: list[int], index=-1):
+def recurse_diff(numbers: list[int], front=True):
     diff = running_diff(numbers)
+    factor = 1 if front else -1
+    index = -1 if front else 0
     if all([a == 0 for a in diff]):
         return diff[index]
     else:
-        return diff[index] - recurse_diff(diff) * index
+        return diff[index] + recurse_diff(diff, False) * factor
 
-def solve_a(data:str, index=-1):
+def solve_a(data:str, front=True):
     lines = data.splitlines()
+    factor = 1 if front else -1
+    index = -1 if front else 0
     values = []
     for line in lines:
         numbers = [int(a) for a in line.split(' ')]
-        values.append(numbers[index] - recurse_diff(numbers, index)*index)
+        values.append(numbers[index] + recurse_diff(numbers, index)*factor)
     return sum(values)
 
 
@@ -37,8 +41,8 @@ if __name__ == "__main__":
     answer_a = solve_a(puzzle.input_data)
     puzzle.answer_a = answer_a
 
-    example_b = solve_a(puzzle.example_data, 1)
+    example_b = solve_a(puzzle.example_data, False)
 
     assert(example_b == 2)
 
-    puzzle.answer_b = solve_a(puzzle.input_data, 1)
+    puzzle.answer_b = solve_a(puzzle.input_data, False)
