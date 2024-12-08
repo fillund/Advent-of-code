0e6849db6debc4305dc7c375082161534dbba47d
Filip Lundberg <lundax@gmail.com>
2024-12-08 21:23:36 +0100

    Day3 B

--- 2024/Day3.py
+++ 2024/Day3.py
@@ -3,8 +3,8 @@ import utils
 import re
 
 PATTERN = r'mul\(\d{1,3},\d{1,3}\)'
-DO_PATTERN = r'do\(\)'
-DONT_PATTERN = r"don't\(\)"
+PATTERN_B =  r"mul\(\d{1,3},\d{1,3}\)|do(?:n\'t)?\(\)"
+
 
 
 def solve_a(data:str):
@@ -15,7 +15,20 @@ def solve_a(data:str):
 
     
 def solve_b(data:str):
-    pass
+    matches = re.findall(PATTERN_B, data)
+    muls = []
+    enable = True
+    for ma in matches:
+        match ma:
+            case "do()":
+                enable = True
+            case "don't()":
+                enable = False
+            case _ if enable:
+                muls.append(utils.numbers(ma))
+    products = [a*b for a,b in muls]
+    return sum(products)
+                
 
 
 if __name__ == "__main__":
@@ -26,8 +39,8 @@ if __name__ == "__main__":
     answer_a = solve_a(puzzle.input_data)
     puzzle.answer_a = answer_a
 
-    # example_b = solve_b(puzzle.example_data)
-    # assert (example_b == 4)
-    # answer_b = solve_b(puzzle.input_data)
-    # puzzle.answer_b = answer_b
+    example_b = solve_b("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+    assert (example_b == 48)
+    answer_b = solve_b(puzzle.input_data)
+    puzzle.answer_b = answer_b
     
