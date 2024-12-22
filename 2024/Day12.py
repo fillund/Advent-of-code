8a2b2ed38c387c5a7d7aa4781e322e16e6ec8495
Filip Lundberg <lundax@gmail.com>
2024-12-22 16:22:36 +0100

    WIP

--- 2024/Day12.py
+++ 2024/Day12.py
@@ -46,7 +46,6 @@ class Region():
     def area(self):
         return len(self.points)
 
-
 def solve_a(data:str):
     grid = utils.parse_grid(data)
     regions:list[Region] = []
@@ -63,24 +62,45 @@ def solve_a(data:str):
             regions.append(Region(next_id, v, [p]))
             next_id+=1
     merged_regions = []
-    print("Merging regions")
+    merge_groups:list[list[int]] = []
+    print("Creating merge groups")
     for r1, r2 in tqdm(it.combinations(regions, 2)):
         if r1.label == r2.label and r1.neighbours()&r2.points:
-            # Merge r2 into r1
-            for p in r2.points:
-                r1.add(p)
-            merged_regions.append(r2.id)
-    print("Removing merged regions")
-    for rm in tqdm(regions.copy()):
-        if rm.id in merged_regions:
-            regions.remove(rm)
+            found = False
+            for group in merge_groups:
+                if r1.id in group:
+                    group.append(r2.id)
+                    found = True
+                    break
+                elif r2.id in group:
+                    group.append(r1.id)
+                    found = True
+                    break
+            if not found:
+                merge_groups.append([r1.id, r2.id])
+    print("Merging regions")
+    for group in merge_groups:
+        regions_to_merge = [r for r in regions if r.id in group]
+        points = []
+        for r in regions_to_merge:
+            points.extend(r.points)
+            regions.remove(r)
+        new_region = Region(regions_to_merge[0].id, regions_to_merge[0].label, points)
+        regions.append(new_region)
+
+    # print("Removing merged regions")
+    # for rm in tqdm(regions.copy()):
+    #     if rm.id in merged_regions:
+    #         regions.remove(rm)
     all_points = []
     for r in regions:
         all_points.extend(r.points)
     counter = Counter(all_points)
     doubles = [p for p,c in counter.items() if c>1]
     print(doubles)
-    assert(counter.total() == len(grid)) 
+    assert(counter.total() == len(grid))
+    for r in regions:
+        print(f"Plant {r.label}: Area:{r.area()} Perimeter:{r.perimiter()} Score: {r.area()*r.perimiter()}")
     return sum([r.area()*r.perimiter() for r in regions])
     
 def solve_b(data:str):
@@ -97,8 +117,28 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE"""
 
+def test_1():
+    data = """OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"""
+    test = solve_a(data)
+    assert (test == 772)
+
+def test_2():
+    data = """AAAA
+BBCD
+BBCC
+EEEC"""
+    test = solve_a(data)
+    assert (test==140)
+
+
 if __name__ == "__main__":
     puzzle = Puzzle(year=2024, day=12)
+    test_1()
+    test_2()
     example_a = solve_a(EXAMPLE)
     assert (example_a == 1930)
     answer_a = solve_a(puzzle.input_data)
