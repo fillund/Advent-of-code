8a1453cd3dd436c896a0ba45d8b4ebf83a1ae31d
Filip Lundberg <lundax@gmail.com>
2024-12-22 18:11:20 +0100

    Day12B WIP

--- 2024/Day12.py
+++ 2024/Day12.py
@@ -5,6 +5,11 @@ from dataclasses import dataclass
 import itertools as it
 from collections import Counter
 from tqdm import tqdm
+from enum import Enum, auto
+
+class SweepState(Enum):
+    INSIDE = auto()
+    OUTSIDE = auto()
 
 @dataclass(unsafe_hash=True)
 class Region():
@@ -43,11 +48,78 @@ class Region():
             region_per += block_per
         return region_per
 
+    def sides(self):
+        sides = 4 # Must at least bound a box
+        bbox = utils.bounding_box(self.points)
+        # Vertical sweep, check x+-1
+        for x in range(bbox.Min_x, bbox.Max_x+1):
+            # TODO: Check start condition
+            left_state = SweepState.INSIDE if Point(x-1, bbox.Min_y) in self.points else SweepState.OUTSIDE
+            right_state = SweepState.INSIDE if Point(x+1, bbox.Min_y) in self.points else SweepState.OUTSIDE
+            for y in range(bbox.Min_y, bbox.Max_y+1):
+                p = Point(x,y)
+                left = Point(x-1, y)
+                right = Point(x+1, y)
+                if p not in self.points:
+                    ## Skip checks if outside to not double count
+                    if left not in self.points:
+                        left_state = SweepState.OUTSIDE
+                    else:
+                        left_state = SweepState.INSIDE
+                    if right not in self.points:
+                        right_state = SweepState.OUTSIDE
+                    else:
+                        right_state = SweepState.INSIDE
+                    continue
+                # Check left
+                if left_state == SweepState.INSIDE and left not in self.points:
+                    sides += 1
+                    left_state = SweepState.OUTSIDE
+                elif left_state == SweepState.OUTSIDE and left in self.points:
+                    left_state = SweepState.INSIDE
+                # Check right
+                if right_state == SweepState.INSIDE and right not in self.points:
+                    sides += 1
+                    right_state = SweepState.OUTSIDE
+                elif right_state == SweepState.OUTSIDE and right in self.points:
+                    right_state = SweepState.INSIDE
+        # Horizontal sweep
+        for y in range(bbox.Min_y, bbox.Max_y+1):
+            up_state = SweepState.INSIDE if Point(bbox.Min_x, y-1) in self.points else SweepState.OUTSIDE
+            down_state = SweepState.INSIDE if Point(bbox.Min_x, y+1) in self.points else SweepState.OUTSIDE
+            for x in range(bbox.Min_x, bbox.Max_x+1):
+                p = Point(x,y)
+                if p not in self.points:
+                    # Skip checks if outside to not double count
+                    if up not in self.points:
+                        up_state = SweepState.OUTSIDE
+                    else:
+                        up_state = SweepState.INSIDE
+                    if down not in self.points:
+                        down_state = SweepState.OUTSIDE
+                    else:
+                        down_state = SweepState.INSIDE
+                    continue
+                up = Point(x, y-1)
+                down = Point(x, y+1)
+                # Check up
+                if up_state == SweepState.INSIDE and up not in self.points:
+                    sides += 1
+                    up_state = SweepState.OUTSIDE
+                elif up_state == SweepState.OUTSIDE and up in self.points:
+                    up_state = SweepState.INSIDE
+                # Check down
+                if down_state == SweepState.INSIDE and down not in self.points:
+                    sides += 1
+                    down_state = SweepState.OUTSIDE
+                elif down_state == SweepState.OUTSIDE and down in self.points:
+                    down_state = SweepState.INSIDE
+        return sides
+
     def area(self):
         return len(self.points)
 
-def solve_a(data:str):
-    grid = utils.parse_grid(data)
+def make_regions(grid):
     regions:list[Region] = []
     explored: set[Point] = set()
     next_id = 0
@@ -68,11 +140,21 @@ def solve_a(data:str):
                 r.add(current)
             q.extend([pn for pn in current.cardinal_neighbours() if grid.get(pn, None) == v and pn not in explored])
         regions.append(r)
+    return regions
 
+def solve_a(data:str):
+    grid = utils.parse_grid(data)
+    regions = make_regions(grid)
     return sum([r.area()*r.perimiter() for r in regions])
     
 def solve_b(data:str):
-    pass
+    grid = utils.parse_grid(data)
+    regions = make_regions(grid)
+    for r in regions:
+        print(f"Plant {r.label} Area {r.area()} Sides {r.sides()}")
+    return sum([r.area()*r.sides() for r in regions])
+
+
                 
 EXAMPLE = """RRRRIICCFF
 RRRRIICCCF
@@ -102,6 +184,24 @@ EEEC"""
     test = solve_a(data)
     assert (test==140)
 
+def test_AB():
+    data = """AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"""
+    test = solve_b(data)
+    assert(test==368)
+
+def test_E():
+    data = """EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE"""
+    test = solve_b(data)
+    assert(test==236)
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2024, day=12)
@@ -111,9 +211,10 @@ if __name__ == "__main__":
     assert (example_a == 1930)
     answer_a = solve_a(puzzle.input_data)
     puzzle.answer_a = answer_a
-
-    example_b = solve_b(puzzle.example_data)
-    assert (example_b == 48)
+    test_AB()
+    test_E()
+    example_b = solve_b(EXAMPLE)
+    assert (example_b == 1206)
     answer_b = solve_b(puzzle.input_data)
     puzzle.answer_b = answer_b
     
