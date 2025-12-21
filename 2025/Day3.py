997c9da744e73d3792928cf5c6d510fcd2aec52e
Filip Lundberg <lundax@gmail.com>
2025-12-21 22:34:44 +0100

    Day 3

--- 2025/Day3.py
+++ 2025/Day3.py
@@ -2,19 +2,41 @@ from aocd.models import Puzzle
 import utils
 import itertools as it
 
+def lint(_list: list) -> int:
+    s = [str(i) for i in _list]
+    return int(''.join(s))
+
+def max_remove(l:list[int]) -> list[int]:
+    vals = []
+    for p in range(len(l)):
+        c = l.copy()
+        c.pop(p)
+        vals.append(c)
+    return max(vals, key=lint)
 
 def bank_joltage(bank:str, active_batteries=2) -> int:
-    combs = it.combinations(bank, active_batteries)
-    return max(map(lambda c: int(''.join(c)), combs))
+    bank_int = [int(c) for c in bank]
+    jolt_list = list(bank_int[-active_batteries:])
+    rev_list = list(reversed(bank_int[:-active_batteries]))
+    assert(len(rev_list)+len(jolt_list) == len(bank_int))
+    for b in rev_list:
+        if b >= jolt_list[0]:
+            jolt_list.insert(0, b)
+            jolt_list = max_remove(jolt_list)
+    return lint(jolt_list)
+
 
 def solve_a(data:str):
     banks = data.splitlines()
-    return sum((bank_joltage(b) for b in banks))
+    joltages = [bank_joltage(b) for b in banks]
+    return sum(joltages)
 
 
     
 def solve_b(data:str):
-    
+    banks = data.splitlines()
+    joltages = [bank_joltage(b, 12) for b in banks]
+    return sum(joltages)
                 
 example_data = """987654321111111
 811111111111119
