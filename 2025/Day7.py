484f96e40f718280cffb039f1f407bfbcedad2c6
Filip Lundberg <lundax@gmail.com>
2025-12-29 22:05:06 +0100

    Day7

--- 2025/Day7.py
+++ 2025/Day7.py
@@ -1,6 +1,6 @@
 from aocd.models import Puzzle
 import utils
-
+from functools import cache
 
 
 
@@ -16,12 +16,48 @@ def solve_a(data:str):
         if "^" in line:
             splitters.append([i for i,c in enumerate(line) if c == "^"])
     
-    return 0
+    num_split = 0
+    for b_t, s_t in zip(beams, splitters):
+        new_beams = set()
+        for b in b_t:
+            if b in s_t:
+                new_beams.add(b-1)
+                new_beams.add(b+1)
+                num_split += 1
+            else:
+                new_beams.add(b)
+        beams.append(list(new_beams))
 
+    return num_split
+
+@cache
+def recurse_b(beam:int, remaining_splitters:tuple[tuple[int]]) -> int:
+    if len(remaining_splitters) == 0:
+        return 1
+
+    for i,s in enumerate(remaining_splitters):
+        if beam in s:
+            remaining = remaining_splitters[i+1:] if len(remaining_splitters) > i else tuple()
+            
+            return recurse_b(beam-1, remaining_splitters=remaining) + recurse_b(beam+1, remaining_splitters=remaining)
+    else:
+        return 1
 
     
 def solve_b(data:str):
-    pass
+    beams: list[list[int]] = [[]]
+    splitters: list[tuple[int]] = []
+    for line in data.splitlines():
+        if not ("S" in line or "^" in line):
+            continue
+        if "S" in line:
+            beams[0] = [line.find("S")]
+        if "^" in line:
+            pos = [i for i,c in enumerate(line) if c == "^"]
+            splitters.append(tuple(pos))  # type: ignore
+
+    timelines = recurse_b(beams[0][0], tuple(splitters))
+    return timelines
                 
 example_data = """.......S.......
 ...............
@@ -49,7 +85,7 @@ if __name__ == "__main__":
     puzzle.answer_a = answer_a
 
     example_b = solve_b(example_data)
-    assert (example_b == 48)
+    assert (example_b == 40)
     answer_b = solve_b(puzzle.input_data)
     puzzle.answer_b = answer_b
     
